af5128236a9ec2f2288c8c437ac6c5acd983a668
TorbenWetter <torben.wetter@t-online.de>
2019-01-10 21:39:00 +0100

    Added OS-based file path separator (probably not even necessary)

--- src/InventoryDisplayAdapter.java
+++ src/InventoryDisplayAdapter.java
@@ -1,5 +1,7 @@
 import greenfoot.*;
 
+import java.io.File;
+
 /**
  * Class used to display an Array of Actors at the bottom of the screen.
  */
@@ -7,21 +9,21 @@ public class InventoryDisplayAdapter extends Actor {
 
     private Actor[] items;
     private Actor[] itemImages;
-    private GreenfootImage emtpySlot = new GreenfootImage("./images/tiles/EmptySlot.png");
+    private GreenfootImage emtpySlot = new GreenfootImage("./images/tiles/EmptySlot.png".replace("/", File.separator));
 
     public InventoryDisplayAdapter(int size) {
         getImage().setTransparency(0);
         items = new Actor[size];
-        itemImages = new Actor[size]; 
-         
+        itemImages = new Actor[size];
+
     }
 
     /**
      * Method of Actor, will be called once the Adapter will be added/inserted (in)to the world.
      * @param world target world
      */
-    protected void addedToWorld(World world){    
-        for (int i = 0; i<itemImages.length; i++ ){  
+    protected void addedToWorld(World world){
+        for (int i = 0; i<itemImages.length; i++ ){
             itemImages[i] = createDummyImageActor(i);
         }
     }
@@ -33,7 +35,7 @@ public class InventoryDisplayAdapter extends Actor {
      * The Array of Actors will be the content that shall be displayed at the bottom of the screen.
      * @param inventory Array that shall be displayed
      */
-    public void update(Actor[] inventory) { 
+    public void update(Actor[] inventory) {
 		int length = Math.max(inventory.length, items.length);
         for (int i = 0; i < length; i++) {
             if (inventory[i] != items[i]) {
@@ -59,13 +61,13 @@ public class InventoryDisplayAdapter extends Actor {
         update(inventorySlots);
     }
 
-    private Actor createDummyImageActor(int i) {        
+    private Actor createDummyImageActor(int i) {
         Actor actor = new Actor() {
         };
         getWorld().addObject(actor, i, getY());
         itemImages[i] = actor;
         actor.setImage(new GreenfootImage(emtpySlot));
-        
+
         return itemImages[i];
 
     }
